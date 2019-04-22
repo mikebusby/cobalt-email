451cb988f7f456da82ccfc37fb7657dddf68f826
mikebusby <hello@mikebusby.email>
2019-04-22 12:25:25 -0400

    Inliner change

--- build/inline-css.js
+++ build/inline-css.js
@@ -20,6 +20,7 @@ module.exports = function(gulp, plugins, config) {
       )
       .pipe(
         inlineCSS({
+          removeStyleTags: false,
           applyTableAttributes: true,
           preserveMediaQueries: true
         })
